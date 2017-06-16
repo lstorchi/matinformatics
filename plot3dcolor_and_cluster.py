7a7c6597013c85906b301a9de61c155afc65e360
Loriano Storchi <loriano@storchi.org>
2017-06-16 12:40:14 +0200

    added label and using centroids as initial geuss

--- plot3dcolor_and_cluster.py
+++ plot3dcolor_and_cluster.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 
-from sklearn.cluster import KMeans
-
 file = ""
 
 if len(sys.argv) == 2:
@@ -19,38 +17,74 @@ lX = []
 color = []
 name = []
 
+CLUST = 8
+
 fp = open(file)
 hdr = fp.readline()
+
+initialcenter = numpy.zeros((CLUST,3))
+
+G = []
+R = []
+B = []
+I = []
+A = []
+V = []
+L = []
+O = []
 
 for l in fp:
     lv = l.split()
 
+    x = float(lv[2])
+    y = float(lv[3])
+    z = float(lv[4])
+
     name.append(lv[0])
     if lv[1] == "G":
         color.append("yellow")
+        G.append((x, y, z))
     elif lv[1] == "R":
         color.append("pink")
+        R.append((x, y, z))
     elif lv[1] == "B":
         color.append("blue")
+        B.append((x, y, z))
     elif lv[1] == "I":
         color.append("grey")
+        I.append((x, y, z))
     elif lv[1] == "A":
         color.append("orange")
+        A.append((x, y, z))
     elif lv[1] == "V":
         color.append("green")
+        V.append((x, y, z))
     elif lv[1] == "L":
         color.append("white")
+        L.append((x, y, z))
     elif lv[1] == "O":
         color.append("red")
+        O.append((x, y, z))
 
-    lX.append([float(lv[2]), float(lv[3]), float(lv[4])])
+    lX.append([x, y, z])
 
 fp.close()
 
+initialcenter[0,:] = numpy.mean(G, axis=0)
+initialcenter[1,:] = numpy.mean(R, axis=0)
+initialcenter[2,:] = numpy.mean(B, axis=0)
+initialcenter[3,:] = numpy.mean(I, axis=0)
+initialcenter[4,:] = numpy.mean(A, axis=0)
+initialcenter[5,:] = numpy.mean(V, axis=0)
+initialcenter[6,:] = numpy.mean(L, axis=0)
+initialcenter[7,:] = numpy.mean(O, axis=0)
+
+print initialcenter
+
 X = numpy.asarray(lX)
+#print X
 
-est = KMeans(n_clusters=8)
-
+est = KMeans(n_clusters=CLUST, init=initialcenter)
 
 est.fit(X)
 labels = est.labels_
@@ -72,6 +106,8 @@ matplotlib.pyplot.clf()
 ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 matplotlib.pyplot.cla()
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, s=100)
+for i, txt in enumerate(name):
+    ax.text(X[i,0],X[i,1],X[i, 2], txt)
 ax.w_xaxis.set_ticklabels([])
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
@@ -84,6 +120,8 @@ matplotlib.pyplot.clf()
 ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 matplotlib.pyplot.cla()
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=labels.astype(numpy.float), s=100)
+for i, txt in enumerate(name):
+    ax.text(X[i,0], X[i,1], X[i, 2], txt)
 ax.w_xaxis.set_ticklabels([])
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
@@ -92,5 +130,3 @@ ax.set_ylabel('GM5+')
 ax.set_zlabel('M2-')
 
 matplotlib.pyplot.show()
-
-
