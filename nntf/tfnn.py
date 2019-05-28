93f2479a698c6f58aa28ecc81dc5bbdf283651ff
Loriano Storchi <loriano@storchi.org>
2019-05-28 11:01:14 +0200

    minor

--- nntf/tfnn.py
+++ nntf/tfnn.py
@@ -58,13 +58,13 @@ def build_seq_model(train_data, train_labels,
 
 ###############################################################################
 
-def plot_history(history):
+def plot_history(history, name = ""):
   hist = pd.DataFrame(history.history)
   hist['epoch'] = history.epoch
   
   plt.figure()
   plt.xlabel('Epoch')
-  plt.ylabel('Mean Abs Error [MPG]')
+  plt.ylabel('Mean Abs Error '+name)
   plt.plot(hist['epoch'], hist['mean_absolute_error'],
            label='Train Error')
   plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
@@ -74,7 +74,7 @@ def plot_history(history):
   
   plt.figure()
   plt.xlabel('Epoch')
-  plt.ylabel('Mean Square Error [$MPG^2$]')
+  plt.ylabel('Mean Square Error [$'+name+'^2$]')
   plt.plot(hist['epoch'], hist['mean_squared_error'],
            label='Train Error')
   plt.plot(hist['epoch'], hist['val_mean_squared_error'],
@@ -86,125 +86,126 @@ def plot_history(history):
 
 ###############################################################################
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f","--file", help="input csv file ", \
-        required=True, type=str)
-parser.add_argument("-s","--showgraphs", help="shows graphs ", \
-        required=False, default=False, action='store_true')
+if __name__ == "__main__":
 
-if len(sys.argv) == 1:
-    parser.print_help()
-    exit(1)
-
-args = parser.parse_args()
-
-filename = args.file
-
-features = pd.read_excel(filename)
-print('The shape of our features is:', features.shape)
-print ("Header: ")
-i = 0
-for name in features.head(1):
-    print("%5d "%(i) + name)
-    i = i + 1
-
-# Remove non numeric data 
-features = features.drop("Name", axis = 1)
-features = features.drop("Class", axis = 1)
-features = features.drop("P", axis = 1)
-
-# remove all non needed 
-# features = features.drop("GM2-", axis = 1)
-# features = features.drop("ep1", axis = 1)
-# features = features.drop("ep2", axis = 1)
-features = features.drop("GM5+", axis = 1)
-features = features.drop("M2-", axis = 1)
-features = features.drop("DV/V(% AFE-FE)", axis = 1)
-features = features.drop("DeltaE", axis = 1)
-features = features.drop("Delta", axis = 1)
-features = features.drop("eta1", axis = 1)
-features = features.drop("eta2", axis = 1)
-features = features.drop("eta3", axis = 1)
-features = features.drop("DV/V(% FE-PA)", axis = 1)
-features = features.drop("ground_state", axis = 1)
-features = features.drop("DV/V(% AFE-PA)", axis = 1)
-
-train_dataset = features.sample(frac=0.8,random_state=0)
-test_dataset = features.drop(train_dataset.index)
-
-train_stats = train_dataset.describe()
-train_stats = train_stats.transpose()
-print(train_stats)
-
-topredict = "DE_sw"
-desc1 = "GM2-"
-desc2 = "ep1"
-desc3 = "ep2"
-fulllist = [desc1, desc2, desc3, topredict]
-
-
-if args.showgraphs:
-    sns.pairplot(train_dataset[fulllist], 
-            diag_kind="kde")
-    plt.show()
-
-test_stats = test_dataset.describe()
-test_stats = test_stats.transpose()
-print(test_stats)
-
-if args.showgraphs:
-    sns.pairplot(test_dataset[fulllist], 
-            diag_kind="kde")
-    plt.show()
-
-train_labels = train_dataset.pop(topredict)
-test_labels = test_dataset.pop(topredict)
-
-train_stats = train_dataset.describe()
-train_stats = train_stats.transpose()
-
-normed_train_data = norm(train_dataset, train_stats)
-normed_test_data = norm(test_dataset, train_stats)
-
-#print(normed_train_data)
-#print(test_dataset)
-
-EPOCHS = 1000
-
-model, history = build_seq_model (normed_train_data, 
-        train_labels, EPOCHS, True)
-
-print (model.summary())
-
-hist = pd.DataFrame(history.history)
-hist['epoch'] = history.epoch
-print(hist.tail())
-
-if args.showgraphs:
-    plot_history(history)
-
-loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=0)
-print("Testing set Mean Abs Error: %5.2f %s"%(mae, topredict))
-
-test_predictions = model.predict(normed_test_data).flatten()
-
-plt.scatter(test_labels, test_predictions)
-plt.xlabel('True Values ['+topredict+']')
-plt.ylabel('Predictions ['+topredict+']')
-plt.axis('equal')
-plt.axis('square')
-plt.xlim([0,plt.xlim()[1]])
-plt.ylim([0,plt.ylim()[1]])
-_ = plt.plot([-100, 100], [-100, 100])
-
-plt.savefig('truevspredicted.png')
-
-error = test_predictions - test_labels
-
-plt.clf()
-plt.cla()
-plt.hist(error, bins = 25)
-plt.xlabel("Prediction Error ["+topredict+"]")
-_ = plt.ylabel("Count")
-
-plt.savefig('predictionerror.png')
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-f","--file", help="input csv file ", \
+          required=True, type=str)
+  parser.add_argument("-s","--showgraphs", help="shows graphs ", \
+          required=False, default=False, action='store_true')
+  
+  if len(sys.argv) == 1:
+      parser.print_help()
+      exit(1)
+  
+  args = parser.parse_args()
+  
+  filename = args.file
+  
+  features = pd.read_excel(filename)
+  print('The shape of our features is:', features.shape)
+  print ("Header: ")
+  i = 0
+  for name in features.head(1):
+      print("%5d "%(i) + name)
+      i = i + 1
+  
+  # Remove non numeric data 
+  features = features.drop("Name", axis = 1)
+  features = features.drop("Class", axis = 1)
+  features = features.drop("P", axis = 1)
+  
+  # remove all non needed 
+  # features = features.drop("GM2-", axis = 1)
+  # features = features.drop("ep1", axis = 1)
+  # features = features.drop("ep2", axis = 1)
+  features = features.drop("GM5+", axis = 1)
+  features = features.drop("M2-", axis = 1)
+  features = features.drop("DV/V(% AFE-FE)", axis = 1)
+  features = features.drop("DeltaE", axis = 1)
+  features = features.drop("Delta", axis = 1)
+  features = features.drop("eta1", axis = 1)
+  features = features.drop("eta2", axis = 1)
+  features = features.drop("eta3", axis = 1)
+  features = features.drop("DV/V(% FE-PA)", axis = 1)
+  features = features.drop("ground_state", axis = 1)
+  features = features.drop("DV/V(% AFE-PA)", axis = 1)
+  
+  train_dataset = features.sample(frac=0.8,random_state=0)
+  test_dataset = features.drop(train_dataset.index)
+  
+  train_stats = train_dataset.describe()
+  train_stats = train_stats.transpose()
+  print(train_stats)
+  
+  topredict = "DE_sw"
+  desc1 = "GM2-"
+  desc2 = "ep1"
+  desc3 = "ep2"
+  fulllist = [desc1, desc2, desc3, topredict]
+  
+  if args.showgraphs:
+      sns.pairplot(train_dataset[fulllist], 
+              diag_kind="kde")
+      plt.show()
+  
+  test_stats = test_dataset.describe()
+  test_stats = test_stats.transpose()
+  print(test_stats)
+  
+  if args.showgraphs:
+      sns.pairplot(test_dataset[fulllist], 
+              diag_kind="kde")
+      plt.show()
+  
+  train_labels = train_dataset.pop(topredict)
+  test_labels = test_dataset.pop(topredict)
+  
+  train_stats = train_dataset.describe()
+  train_stats = train_stats.transpose()
+  
+  normed_train_data = norm(train_dataset, train_stats)
+  normed_test_data = norm(test_dataset, train_stats)
+  
+  #print(normed_train_data)
+  #print(test_dataset)
+  
+  EPOCHS = 1000
+  
+  model, history = build_seq_model (normed_train_data, 
+          train_labels, EPOCHS, True)
+  
+  print (model.summary())
+  
+  hist = pd.DataFrame(history.history)
+  hist['epoch'] = history.epoch
+  print(hist.tail())
+  
+  if args.showgraphs:
+      plot_history(history, ttopredict)
+  
+  loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=0)
+  print("Testing set Mean Abs Error: %5.2f %s"%(mae, topredict))
+  
+  test_predictions = model.predict(normed_test_data).flatten()
+  
+  plt.scatter(test_labels, test_predictions)
+  plt.xlabel('True Values ['+topredict+']')
+  plt.ylabel('Predictions ['+topredict+']')
+  plt.axis('equal')
+  plt.axis('square')
+  plt.xlim([0,plt.xlim()[1]])
+  plt.ylim([0,plt.ylim()[1]])
+  _ = plt.plot([-100, 100], [-100, 100])
+  
+  plt.savefig('truevspredicted.png')
+  
+  error = test_predictions - test_labels
+  
+  plt.clf()
+  plt.cla()
+  plt.hist(error, bins = 25)
+  plt.xlabel("Prediction Error ["+topredict+"]")
+  _ = plt.ylabel("Count")
+  
+  plt.savefig('predictionerror.png')
