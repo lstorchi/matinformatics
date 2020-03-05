8857033896eb57e34742ae6bedfe81db30d5817b
Loriano Storchi <loriano@storchi.org>
2020-03-05 16:03:38 +0100

    start adding option for basic features list

--- generatefeats.py
+++ generatefeats.py
@@ -12,6 +12,10 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("-f","--file", help="input xlsx file ", \
           required=True, type=str)
+  parser.add_argument("-b","--basicfeatures", \
+          help="input ; separated list of basic featuresto combine \n" + \
+          "   each feature has an associated type (i.e. \"IP[1];EA[1];Z[2]\"", \
+          required=True, type=str)
  
   args = parser.parse_args()
   
@@ -20,14 +24,19 @@ if __name__ == "__main__":
   xls = pd.ExcelFile(filename)
 
   atomicdata = pd.read_excel(xls, "Atomic Data")
+  basicfeatureslist = args.basicfeatures.split(";")
+  for b in basicfeatureslist:
+      newb = b.split("[")
+      #if len(newb) != 2:
+
 
   try:
       formula = "(IP + EA)/Z"
       newf = matinfmod.get_new_feature(atomicdata, formula)
 
-      print (formula)
-      for v in newf:
-          print("%10.5f"%v)
+      #print (formula)
+      #for v in newf:
+      #    print("%10.5f"%v)
 
   except NameError as err:
       print(err)
