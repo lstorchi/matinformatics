33276d0a7ca60eb7641b773777bc5325785ebe1f
Loriano Storchi <loriano@storchi.org>
2020-04-27 15:02:42 +0200

    test load balancing

--- run_mutiple.sh
+++ run_mutiple.sh
@@ -1,6 +1,6 @@
 
-export BASE=4000
-export MAX=16
+export BASE=150
+export MAX=32
 export START=0
 
 for i in $(seq 1 $MAX)
@@ -8,4 +8,5 @@ do
   export END=$(($START + $BASE))
   python3 generate2Dfeats.py -f feature_rmse.csv -n 40 -k newadata.pkl -N 1 -r "$START:$END" 1> out_$i 2> err_$i &
   export START=$(($START + $BASE))
+  export BASE=$((2 * $BASE))
 done
