131f5765f7113772c03615abd3471fa71742ce20
Loriano Storchi <loriano@storchi.org>
2020-04-28 07:41:27 -0400

    update

--- run_mutiple.sh
+++ run_mutiple.sh
@@ -5,15 +5,18 @@ then
   exit
 fi
 
+export UPDATEVAL=0
+
 export BASEVAL=$1
 export STARTPROC=$2
 export NUMPROCS=$3
 export START=$4
 export MAXNUM=$5
 
+export BASE=$BASEVAL
+
 for i in $(seq $STARTPROC $NUMPROCS)
 do
-  export BASE=$BASEVAL
   export END=$(($START + $BASE))
   if [ $END -ge $MAXNUM ]; then
     python3 generate2Dfeats.py -S -f feature_rmse.csv -n 20 -k newadata.pkl -N 1 -r "$START:$MAXNUM" -F $MAXNUM 1> out_$i 2> err_$i &
@@ -25,5 +28,8 @@ do
   echo "Process " $i " will start from " $START " to " $END " of " $MAXNUM
   echo ""
   export START=$(($START + $BASE))
-  export BASE=$(($i * $BASE))
+  if [ $UPDATEVAL -eq 1 ]
+  then 
+    export BASE=$(($i * $BASE))
+  fi
 done
