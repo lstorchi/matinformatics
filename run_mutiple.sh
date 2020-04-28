1c5aa1900fcd91bea7b976a45f1d99c3f3907ef9
Loriano Storchi <loriano@storchi.org>
2020-04-28 05:26:00 -0400

    script to quickly run on multiple nodes

--- run_mutiple.sh
+++ run_mutiple.sh
@@ -1,5 +1,5 @@
 
-export BASEVAL=500
+export BASEVAL=300
 export NUMPROCS=32
 export START=0
 export MAXNUM=40000
@@ -9,12 +9,12 @@ do
   export BASE=$BASEVAL
   export END=$(($START + $BASE))
   if [ $END -ge $MAXNUM ]; then
-    python3 generate2Dfeats.py -S -f feature_rmse.csv -n 40 -k newadata.pkl -N 1 -r "$START:$MAXNUM" -F $MAXNUM 1> out_$i 2> err_$i &
+    python3 generate2Dfeats.py -S -f feature_rmse.csv -n 20 -k newadata.pkl -N 1 -r "$START:$MAXNUM" -F $MAXNUM 1> out_$i 2> err_$i &
     echo "Process " $i " will start from " $START " to " $MAXNUM " of " $MAXNUM
     echo ""
     exit
   fi
-  python3 generate2Dfeats.py -S -f feature_rmse.csv -n 40 -k newadata.pkl -N 1 -r "$START:$END" -F $MAXNUM 1> out_$i 2> err_$i &
+  python3 generate2Dfeats.py -S -f feature_rmse.csv -n 20 -k newadata.pkl -N 1 -r "$START:$END" -F $MAXNUM 1> out_$i 2> err_$i &
   echo "Process " $i " will start from " $START " to " $END " of " $MAXNUM
   echo ""
   export START=$(($START + $BASE))
