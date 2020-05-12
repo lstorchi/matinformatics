6a4d167f16780c650519b63ff0347f459c538504
Loriano Storchi <loriano@storchi.org>
2020-05-12 15:45:28 +0200

    start adding new codes

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 
 Just some basic test used in a cooperative work 
 related to material informatics 
+
+Run using separate Data and AtomicData
+
+  # generate features
+  python3 generatefeats.py  -f ./data/Data.xlsx -b "IP[1];EA[1];Z[0];rs[2];rp[2];rd[2];HOMOKS[3];LUMOKS[3]" -d 10
+
+  # select best 1D features
+  python3 ffilter.py -f newadata.pkl -n 50
+
+  # generate 2D features starting from the best 1D and select the best ones
+  python3 generate2Dfeats.py -f feature_rmse.csv -n 50 -k newadata.pkl --numoffeatures 1000
+
+  # generate 3D features starting from the best 1D and select the best ones
+  python3 generate3Dfeats.py  -f feature_rmse.csv -n 50 -k newadata.pkl --numoffeatures 10
+
+  # collect output and 
+  python3 23Dfeatsexctratand.py -d ./2DFeatures/ -k newadata.pkl
+  python3 23Dfeatsexctratand.py -d ./3DFeatures/ -k newadata.pkl --set3Don
+
+Run ising a single Dataset no Atomic Data 
+
+  # generates basic features
+  python3 generatefeats_matdata.py -b "spacegroup[1];number_of_total_atoms[1];percent_atom_al[2];percent_atom_ga[2];percent_atom_in[2];lattice_vector_1_ang[3];lattice_vector_2_ang[3];lattice_vector_3_ang[3];lattice_angle_alpha_degree[4];lattice_angle_beta_degree[4];lattice_angle_gamma_degree[4];formation_energy_ev_natom[5];bandgap_energy_ev[5];avg_dist_Ga[6];avg_dist_Al[6];avg_dist_In[6];V_alpha[1];Density[7]" -f ./data/updated_dataset.xlsx
+
---
+++ generatefeats_matdata.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+#import matplotlib
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+import argparse
+import sys
+import os
+
+sys.path.append("./common/")
+
+import matinfmod 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--file", help="input xlsx file ", \
+                        required=True, type=str)
+    parser.add_argument("-b","--basicfeatures", \
+                        help="input ; separated list of basic featuresto combine \n" + \
+                        "   each feature has an associated type (i.e. \"IP[1];EA[1];Z[2]\"", \
+                        required=True, type=str)
+    
+    args = parser.parse_args()
+    
+    filename = args.file
+    
+    xls = pd.ExcelFile(filename)
+    
+    data = pd.read_excel(xls)
+    basicfeatureslist = args.basicfeatures.split(";")
+    
+    basicfeaturesdict = {}
+    for b in basicfeatureslist:
+        newb = b.split("[")
+        if len(newb) != 2:
+            print("Error in basicfeatures format")
+            exit(1)
+            
+        classe = newb[1].replace("]", "")
+        name = newb[0]
+        
+        if not (name in data.columns):
+            print("Error feature ", name, "not present")
+            for bf in data.columns:
+                print("   ", bf )
+            exit(1)
+            
+        if not (classe in basicfeaturesdict):
+            basicfeaturesdict[classe] = []
+            basicfeaturesdict[classe].append(name)
+        else:
+            basicfeaturesdict[classe].append(name)
+    
+    try:
+        print("Here")
+    except NameError as err:
+        print(err)
+
