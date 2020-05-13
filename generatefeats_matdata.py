64105a0c5cf2b1e6cd09a56baec8c416ca1d7ce8
Loriano Storchi <loriano@storchi.org>
2020-05-13 17:54:13 +0200

    new features are producted checking also math expections

--- generatefeats_matdata.py
+++ generatefeats_matdata.py
@@ -6,12 +6,35 @@ import numpy as np
 #import seaborn as sns
 
 import argparse
+import math
 import sys
 import os
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 sys.path.append("./common/")
 
 import matinfmod 
+
+# get from here https://stackoverflow.com/questions/17778394/list-highest-correlation-pairs-from-a-large-correlation-matrix-in-pandas
+def get_redundant_pairs(df):
+    '''Get diagonal and lower triangular pairs of correlation matrix'''
+    pairs_to_drop = set()
+    cols = df.columns
+    for i in range(0, df.shape[1]):
+        for j in range(0, i+1):
+            pairs_to_drop.add((cols[i], cols[j]))
+
+    return pairs_to_drop
+
+def get_top_abs_correlations(df, n=5):
+
+    au_corr = df.corr().abs().unstack()
+    labels_to_drop = get_redundant_pairs(df)
+    au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
+
+    return au_corr[0:n]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,16 +44,48 @@ if __name__ == "__main__":
                         help="input ; separated list of basic featuresto combine \n" + \
                         "   each feature has an associated type (i.e. \"IP[1];EA[1];Z[2]\"", \
                         required=True, type=str)
+    parser.add_argument("-d", "--dumponly", \
+                        help="to dump only the first N formulas",
+                        required=False, type=int, default=-1)
+    parser.add_argument("-v", "--verbose", \
+                        help="verbose mode", action="store_true",
+                        required=False, default=False)
+ 
     
     args = parser.parse_args()
     
     filename = args.file
     
     xls = pd.ExcelFile(filename)
-    
-    data = pd.read_excel(xls)
+    data = pd.read_excel(xls, index_col=0)
     basicfeatureslist = args.basicfeatures.split(";")
-    
+
+    data = data.drop(columns=['id', "Centre_of_mass_cordinates"])
+
+    #for V_alpha in data["V_alpha"]:
+    #   print(V_alpha)
+    #   print(math.exp(V_alpha))
+
+    corrlimit = 0.95
+
+    atleastone = False
+    print("Top Absolute Correlations")
+    topcorr = get_top_abs_correlations(data, 5)
+    for key, value in topcorr.items():
+       print("%30s %30s %10.6f"%(key[0], key[1], value))
+       if (value > corrlimit):
+          atleastone = True
+
+    if atleastone:
+        print("High correlationd found among basic features")
+        exit(1)
+
+    #corrmat = data.corr()
+    #ax = sns.heatmap(corrmat, annot = True, vmin=-1, vmax=1, center= 0)
+    #ax = sns.heatmap(corrmat, vmin=-1, vmax=1, center= 0)
+    #plt.savefig("cooheatmap.png")
+    #plt.show()
+
     basicfeaturesdict = {}
     for b in basicfeatureslist:
         newb = b.split("[")
@@ -64,7 +119,41 @@ if __name__ == "__main__":
             fp.write(f + "\n")
         fp.close()
         print("Generated ", len(formulas) ," formulas...")
+
+        print ("Start generating features...")
+        last = args.dumponly
+        i = 1
+        max = last  
+        if last < 0:
+            max = len(formulas)
+
+        newdataframe = {}
+            
+        for formula in formulas[0:last]:
+            if not args.verbose:
+                matinfmod.progress_bar(i, max)
+                i = i + 1
+            #print(formula)
+
+            try:
+                newf = matinfmod.get_new_feature(data, formula)
+            except OverflowError:
+                print("Math error in formula (overflow)", formula)
+            except ZeroDivisionError:
+                print("Math error in formula (division by zero)", formula)
+
+            newdataframe[formula] = newf
+        
+        if not args.verbose:
+            print()
+            
+        newatomicdata = pd.DataFrame.from_dict(newdataframe)      
+        print ("Produced ", newatomicdata.size , " data features")
+
+ 
  
     except NameError as err:
         print(err)
+
+
 
