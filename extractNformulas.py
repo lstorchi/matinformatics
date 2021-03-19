804cd786c0191428badb4028f6749b5559574104
Loriano Storchi <loriano@storchi.org>
2021-03-19 11:48:07 +0100

    backup

---
+++ extractNformulas.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+
+sys.path.append("./common/")
+
+import matinfmod 
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--file", help="input csv file ", \
+            required=True, type=str)
+    parser.add_argument("-n", help="input formulas to dump defaul=10 ", \
+            required=False, type=int, default=10)
+ 
+    
+    args = parser.parse_args()
+    
+    filename = args.file
+
+    df = pd.read_csv(filename)
+
+    df = df.sort_values('rmse')
+
+    selected = df.head(args.n)
+
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
+
+    print(selected["formulas"])
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ];
+export METHOD = 1
+
+if [ "$#" -ne 6 ];
 then
   echo "illegal number of parameters"
-  echo "usage: $0 DATAFILE ATOMICFEATURES LABEL DATASHEETNAME ATOMICSHEETNAME"
+  echo "usage: $0 DATAFILE ATOMICFEATURES LABEL DATASHEETNAME ATOMICSHEETNAME METHOD"
   exit
 fi
 
@@ -12,8 +14,10 @@ export ATOMICFEATURES=$2
 export LABEL=$3
 export DATASHEETNAME=$4
 export ATOMICSHEETNAME=$5
+export METHOD=$6
 
-python3 generatefeats_pelect.py  -f $DATAFILE -b $ATOMICFEATURES -j -m 1 -l $LABEL \
+python3 generatefeats_pelect.py  -f $DATAFILE -b $ATOMICFEATURES -j -m $METHOD -l $LABEL \
   --sheetnames "$DATASHEETNAME,$ATOMICSHEETNAME"
 python3 ffilter.py -f newadata.pkl -n 50 -i "$DATAFILE,$LABEL,$DATASHEETNAME"
 python3 checksingleformula.py -n 1000 -f newadata.pkl  -i "$DATAFILE,$LABEL,$DATASHEETNAME"
+python3 ./extractNformulas.py -f feature_rmse.csv
