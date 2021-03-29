e237d9adbb3d5656a2ba0c336acef00186585942
Loriano Storchi <loriano@storchi.org>
2021-03-29 11:55:11 +0200

    adding utility NB

--- extractNformulas.py
+++ extractNformulas.py
@@ -14,18 +14,23 @@ if __name__ == "__main__":
             required=True, type=str)
     parser.add_argument("-n", help="input formulas to dump defaul=10 ", \
             required=False, type=int, default=10)
- 
-    
+    parser.add_argument("--abc", help="extract formula having A B C ", \
+            required=False, action="store_true", default=False)
+
     args = parser.parse_args()
     
     filename = args.file
 
     df = pd.read_csv(filename)
 
-    df = df.sort_values('mse')
+    if args.abc:
+        dfa = df[df['formulas'].str.contains("_A")]
+        dfb = dfa[dfa['formulas'].str.contains("_B")]
+        df = dfb[dfb['formulas'].str.contains("_C")]
+
+    df = df.sort_values('rmse')
 
     selected = df.head(args.n)
-
 
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
